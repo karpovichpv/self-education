7514feb1eb5a275b3da6ae52fb903a9df40d14d2
karpovichpv <karpovichpv@yandex.ru>
2023-08-03 18:37:31 +0300

    ASP.NET Core. Begin EF practice

---
+++ asp.net core/asp.net core + angular/6_ef/DutchTreat/Data/DutchContext.cs
@@ -0,0 +1,18 @@
+﻿using DutchTreat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DutchTreat.Data
+{
+    public class DutchContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer();
+        }
+    }
+}
