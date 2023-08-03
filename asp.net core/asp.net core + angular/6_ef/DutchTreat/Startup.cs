b37359b82b13bcbcff42340b0da2549bc3502e90
karpovichpv <karpovichpv@yandex.ru>
2023-08-03 18:38:33 +0300

    ASP.NET Core. Fix Startup.cs file

--- asp.net core/asp.net core + angular/6_ef/DutchTreat/Startup.cs
+++ asp.net core/asp.net core + angular/6_ef/DutchTreat/Startup.cs
@@ -2,7 +2,6 @@ using DutchTreat.Data;
 using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -14,10 +13,7 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DutchContext>(cfg =>
-            {
-                cfg.UseSqlServer();
-            });
+            services.AddDbContext<DutchContext>();
             services.AddTransient<IMailService, NullMailService>();
 
             services.AddControllersWithViews()
