23a60797120e9b9c65b5f2bfe199033c01eb672f
Pavel Karpovich <karpovich.pavel@gmail.com>
2023-08-03 22:28:31 +0300

    ASP.NET Core. Initialize db

--- asp.net core/asp.net core + angular/6_ef/DutchTreat/Data/DutchContext.cs
+++ asp.net core/asp.net core + angular/6_ef/DutchTreat/Data/DutchContext.cs
@@ -1,10 +1,19 @@
 ﻿using DutchTreat.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
 
 namespace DutchTreat.Data
 {
     public class DutchContext : DbContext
     {
+        private readonly IConfiguration _configuration;
+
+        public DutchContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
 
@@ -12,7 +21,28 @@ namespace DutchTreat.Data
         {
             base.OnConfiguring(optionsBuilder);
 
-            optionsBuilder.UseSqlServer();
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DutchContextDb"));
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>()
+              .Property(p => p.Price)
+              .HasColumnType("money");
+
+            modelBuilder.Entity<OrderItem>()
+              .Property(o => o.UnitPrice)
+              .HasColumnType("money");
+
+            modelBuilder.Entity<Order>()
+              .HasData(new Order()
+              {
+                  Id = 1,
+                  OrderDate = DateTime.UtcNow,
+                  OrderNumber = "12345"
+              });
+        }
+
     }
 }
--- asp.net core/asp.net core + angular/6_ef/DutchTreat/Program.cs
+++ asp.net core/asp.net core + angular/6_ef/DutchTreat/Program.cs
@@ -1,26 +1,32 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace DutchTreat
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    public class Program
     {
-      CreateHostBuilder(args).Build().Run();
-    }
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            });
-  }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(AddConfiguration)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+
+        private static void AddConfiguration(HostBuilderContext context, IConfigurationBuilder builder)
+        {
+            builder.Sources.Clear();
+
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json", false, true)
+                .AddEnvironmentVariables();
+        }
+    }
 }
