fa7997a919294fab462a1a9539a912e5848c3a3d
karpovichpv <karpovichpv@yandex.ru>
2023-08-30 09:25:15 +0300

    ASP.NET Core. Finish with Angular first module

--- asp.net core/asp.net core + angular/10_angular/DutchTreat/client/src/app/app.module.ts
+++ asp.net core/asp.net core + angular/10_angular/DutchTreat/client/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import ProductListView from './views/productListView.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ProductListView
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
