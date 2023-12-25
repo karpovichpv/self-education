fdbb7433e8e68d7b459664f208ec15369f410b41
Pavel Karpovich <karpovich.pavel@gmail.com>
2023-12-25 18:51:04 +0300

    Add the ShortDescription property to the Product object

--- asp.net core/asp.net core (shop)/Rocky/Views/Models/Product.cs
+++ asp.net core/asp.net core (shop)/Rocky/Views/Models/Product.cs
@@ -12,6 +12,8 @@ namespace Rocky.Models
         public string Name { get; set; }
         public string Description { get; set; }
 
+        public string ShortDescription { get; set; }
+
         [Range(1, int.MaxValue)]
         public double Price { get; set; }
         public string Image { get; set; }
