495e061c9d77decaaa0d1af12f21fe8aca1cf2c6
Pavel Karpovich <karpovich.pavel@gmail.com>
2023-12-27 21:42:25 +0300

    Add category buttons and products cards

--- asp.net core/asp.net core (shop)/Rocky/Controllers/HomeController.cs
+++ asp.net core/asp.net core (shop)/Rocky/Controllers/HomeController.cs
@@ -1,32 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Rocky.Data;
 using Rocky.Models;
+using Rocky.Models.ViewModels;
 using System.Diagnostics;
 
 namespace Rocky.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
+		{
+			_logger = logger;
+			_db = db;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			HomeViewModel homeViewModel = new HomeViewModel()
+			{
+				Products = _db.Product.Include(u => u.Category).Include(u => u.ApplicationType),
+				Categories = _db.Category,
+			};
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+			return View(homeViewModel);
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
