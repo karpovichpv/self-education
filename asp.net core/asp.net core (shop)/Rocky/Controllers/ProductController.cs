49b72b8db73cdfc33b60961b296271044c9d1634
Pavel Karpovich <karpovich.pavel@gmail.com>
2023-12-28 21:20:24 +0300

    Add filtration for the main page

--- asp.net core/asp.net core (shop)/Rocky/Controllers/ProductController.cs
+++ asp.net core/asp.net core (shop)/Rocky/Controllers/ProductController.cs
@@ -7,146 +7,173 @@ using Rocky.Models.ViewModels;
 
 namespace Rocky.Controllers
 {
-    public class ProductController : Controller
-    {
-        private readonly ApplicationDbContext _db;
-        private readonly IWebHostEnvironment _webHostEnvironment;
+	public class ProductController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+		private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ProductController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
-        {
-            _db = db;
-            _webHostEnvironment = webHostEnvironment;
-        }
+		public ProductController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
+		{
+			_db = db;
+			_webHostEnvironment = webHostEnvironment;
+		}
 
-        public IActionResult Index()
-        {
-            IEnumerable<Product> objList = _db.Product
-                .Include(u => u.Category)
-                .Include(u => u.ApplicationType);
+		public IActionResult Index()
+		{
+			IEnumerable<Product> objList = _db.Product
+				.Include(u => u.Category)
+				.Include(u => u.ApplicationType);
 
-            return View(objList);
-        }
+			return View(objList);
+		}
 
-        public IActionResult Upsert(int? id)
-        {
-            ProductViewModel productViewModel = new()
-            {
-                Product = new Product(),
-                CategorySelectList = _db.Category.Select(i => new SelectListItem
-                {
-                    Text = i.Name,
-                    Value = i.Id.ToString()
-                }),
-                ApplicationTypeSelectList = _db.ApplicationType.Select(i => new SelectListItem
-                {
-                    Text = i.Name,
-                    Value = i.Id.ToString()
-                }),
-            };
+		public IActionResult Upsert(int? id)
+		{
+			ProductViewModel productViewModel = new()
+			{
+				Product = new Product(),
+				CategorySelectList = _db.Category.Select(i => new SelectListItem
+				{
+					Text = i.Name,
+					Value = i.Id.ToString()
+				}),
+				ApplicationTypeSelectList = _db.ApplicationType.Select(i => new SelectListItem
+				{
+					Text = i.Name,
+					Value = i.Id.ToString()
+				}),
+			};
 
-            if (id == null)
-            {
-                return View(productViewModel);
-            }
-            else
-            {
-                productViewModel.Product = _db.Product.Find(id);
-                if (productViewModel.Product == null)
-                {
-                    return NotFound();
-                }
+			if (id == null)
+			{
+				return View(productViewModel);
+			}
+			else
+			{
+				productViewModel.Product = _db.Product.Find(id);
+				if (productViewModel.Product == null)
+				{
+					return NotFound();
+				}
 
-                return View(productViewModel);
-            }
-        }
+				return View(productViewModel);
+			}
+		}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Upsert(ProductViewModel productViewModel)
-        {
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Upsert(ProductViewModel productViewModel)
+		{
 
-            if (ModelState.IsValid)
-            {
-                var files = HttpContext.Request.Form.Files;
-                string webRootPath = _webHostEnvironment.WebRootPath;
+			if (ModelState.IsValid)
+			{
+				var files = HttpContext.Request.Form.Files;
+				string webRootPath = _webHostEnvironment.WebRootPath;
 
-                if (productViewModel.Product.Id == 0)
-                {
-                    // Creating
+				if (productViewModel.Product.Id == 0)
+				{
+					// Creating
 
-                    string upload = webRootPath + WebConstants.ImagePath;
-                    string fileName = Guid.NewGuid().ToString();
-                    string extension = Path.GetExtension(files[0].FileName);
+					string upload = webRootPath + WebConstants.ImagePath;
+					string fileName = Guid.NewGuid().ToString();
+					string extension = Path.GetExtension(files[0].FileName);
 
-                    using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
-                    {
-                        files[0].CopyTo(fileStream);
-                    }
+					using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+					{
+						files[0].CopyTo(fileStream);
+					}
 
-                    productViewModel.Product.Image = fileName + extension;
+					productViewModel.Product.Image = fileName + extension;
 
-                    _db.Product.Add(productViewModel.Product);
-                }
-                else
-                {
-                    // Updating
-                }
+					_db.Product.Add(productViewModel.Product);
+				}
+				else
+				{
+					var objFromDb = _db.Product.AsNoTracking().FirstOrDefault(u => u.Id == productViewModel.Product.Id);
 
-                productViewModel.CategorySelectList = _db.Category.Select(i => new SelectListItem
-                {
-                    Text = i.Name,
-                    Value = i.Id.ToString()
-                });
-                productViewModel.ApplicationTypeSelectList = _db.ApplicationType.Select(i => new SelectListItem
-                {
-                    Text = i.Name,
-                    Value = i.Id.ToString()
-                });
+					if (files.Count > 0)
+					{
+						string upload = webRootPath + WebConstants.ImagePath;
+						string fileName = Guid.NewGuid().ToString();
+						string extension = Path.GetExtension(files[0].FileName);
 
-                _db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(productViewModel);
-        }
+						var oldFile = Path.Combine(upload, objFromDb.Image);
 
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-                return NotFound();
+						if (System.IO.File.Exists(oldFile))
+						{
+							System.IO.File.Delete(oldFile);
+						}
 
-            Product product = _db.Product
-                .Include(u => u.Category)
-                .Include(u => u.ApplicationType)
-                .FirstOrDefault(u => u.Id == id);
+						using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+						{
+							files[0].CopyTo(fileStream);
+						}
 
-            if (product == null)
-                return NotFound();
+						productViewModel.Product.Image = fileName + extension;
+					}
+					else
+					{
+						productViewModel.Product.Image = objFromDb.Image;
+					}
+					_db.Product.Update(productViewModel.Product);
 
-            return View(product);
-        }
+				}
 
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeletePost(int? id)
-        {
-            var obj = _db.Product.Find(id);
+				productViewModel.CategorySelectList = _db.Category.Select(i => new SelectListItem
+				{
+					Text = i.Name,
+					Value = i.Id.ToString()
+				});
+				productViewModel.ApplicationTypeSelectList = _db.ApplicationType.Select(i => new SelectListItem
+				{
+					Text = i.Name,
+					Value = i.Id.ToString()
+				});
 
-            if (obj == null)
-            {
-                return NotFound();
-            }
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(productViewModel);
+		}
 
-            string upload = _webHostEnvironment.WebRootPath + WebConstants.ImagePath;
-            string oldFile = Path.Combine(upload, obj.Image);
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+				return NotFound();
 
-            if (System.IO.File.Exists(oldFile))
-            {
-                System.IO.File.Delete(oldFile);
-            }
+			Product product = _db.Product
+				.Include(u => u.Category)
+				.Include(u => u.ApplicationType)
+				.FirstOrDefault(u => u.Id == id);
 
-            _db.Product.Remove(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-    }
+			if (product == null)
+				return NotFound();
+
+			return View(product);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public IActionResult DeletePost(int? id)
+		{
+			var obj = _db.Product.Find(id);
+
+			if (obj == null)
+			{
+				return NotFound();
+			}
+
+			string upload = _webHostEnvironment.WebRootPath + WebConstants.ImagePath;
+			string oldFile = Path.Combine(upload, obj.Image);
+
+			if (System.IO.File.Exists(oldFile))
+			{
+				System.IO.File.Delete(oldFile);
+			}
+
+			_db.Product.Remove(obj);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
 }
