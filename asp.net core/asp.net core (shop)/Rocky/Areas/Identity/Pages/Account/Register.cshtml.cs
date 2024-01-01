e8882e5d7ed16b4b69573f35315f4a8f47143ade
Pavel Karpovich <karpovich.pavel@gmail.com>
2024-01-01 20:49:54 +0300

    Add PhoneNumber and FullName to the Register page

--- asp.net core/asp.net core (shop)/Rocky/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ asp.net core/asp.net core (shop)/Rocky/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -2,22 +2,16 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Text.Encodings.Web;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
-using Microsoft.Extensions.Logging;
+using Rocky.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.Encodings.Web;
 
 namespace Rocky.Areas.Identity.Pages.Account
 {
@@ -97,6 +91,9 @@ namespace Rocky.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            public string FullName { get; set; }
+            public string PhoneNumber { get; set; }
         }
 
 
@@ -112,7 +109,10 @@ namespace Rocky.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = CreateUser();
+                ApplicationUser user = CreateUser();
+
+                user.PhoneNumber = Input.PhoneNumber;
+                user.FullName = Input.FullName;
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
@@ -154,11 +154,11 @@ namespace Rocky.Areas.Identity.Pages.Account
             return Page();
         }
 
-        private IdentityUser CreateUser()
+        private ApplicationUser CreateUser()
         {
             try
             {
-                return Activator.CreateInstance<IdentityUser>();
+                return Activator.CreateInstance<ApplicationUser>();
             }
             catch
             {
--- asp.net core/asp.net core (shop)/Rocky/WebConstants.cs
+++ asp.net core/asp.net core (shop)/Rocky/WebConstants.cs
@@ -2,4 +2,7 @@
 {
     public const string ImagePath = @"\images\product\";
     public const string SessionCart = "ShoppingCartSession";
+
+    public const string AdminRole = "Admin";
+    public const string CustomerRole = "Customer";
 }
