a16667191f1886711c29aa4325668b8d103335d8
Pavel Karpovich <karpovich.pavel@gmail.com>
2024-01-03 08:09:27 +0300

    Begin work on the CartController

---
+++ asp.net core/asp.net core (shop)/Rocky/Controllers/CartController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+
+namespace Rocky.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<ShoppingCart> shoppingCart = new List<ShoppingCart>();
+
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WebConstants.SessionCart) != null
+
+            return View();
+        }
+    }
+}
