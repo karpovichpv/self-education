45fd7bd78a1ec174f7672a002d65a30f753ac515
Pavel Karpovich <karpovich.pavel@gmail.com>
2024-02-21 22:30:52 +0300

    Add the second ThreadJoinMethod2

---
+++ .net threads + async await/.net threads/ThreadJointMethod/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        // Create of a new thread
+        Thread thread = new(new ThreadStart(Function));
+        thread.Start();
+
+        // The primary thread is waiting while the secondary will be finished
+        thread.Join();
+
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        for (int i = 0; i < 160; i++)
+        {
+            Thread.Sleep(20);
+            Console.Write("-");
+        }
+
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine("\nPrimary thread was finished");
+    }
+
+    private static void Function()
+    {
+        Console.WriteLine($"ID of the secondary thread: {Thread.CurrentThread.ManagedThreadId}");
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+        for (int i = 0; i < 160; i++)
+        {
+            Thread.Sleep(20);
+            Console.Write(".");
+        }
+
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine("The secondary thread was finished");
+    }
+}
