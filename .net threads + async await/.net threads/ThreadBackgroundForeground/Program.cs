5404a45778ee8da8f2c8dee6cee4866c08bc348f
Pavel Karpovich <karpovich.pavel@gmail.com>
2024-02-22 21:27:49 +0300

    Add ThreadBackground example project

---
+++ .net threads + async await/.net threads/ThreadBackgroundForeground/Program.cs
@@ -0,0 +1,25 @@
+﻿
+public class Program
+{
+    public static void Main()
+    {
+        var thread = new Thread(Procedure);
+        //thread.IsBackground = true;
+        thread.Start();
+
+        Thread.Sleep(500);
+
+        Console.WriteLine("\n Finishing of the primary thread");
+    }
+
+    private static void Procedure()
+    {
+        for (int i = 0; i < 500; i++)
+        {
+            Thread.Sleep(10);
+            Console.Write(".");
+        }
+
+        Console.WriteLine("\n Finishing of the secondary thread");
+    }
+}
