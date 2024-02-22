4221cddb334a48edcd421ee1500018028b86d8ee
Pavel Karpovich <karpovich.pavel@gmail.com>
2024-02-22 20:30:48 +0300

    Add an example of accessing to the static field from the thread

---
+++ .net threads + async await/.net threads/TreadsAccessToStaticField/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Program
+{
+    // [ThreadStatic]
+    private static int _counter;
+
+    public static void Main()
+    {
+        Thread thread = new(Method);
+        thread.Start();
+        thread.Join();
+
+        Console.WriteLine("Primary thread was finished");
+
+        Console.ReadKey();
+    }
+
+    public static void Method()
+    {
+        if (_counter < 100)
+        {
+            _counter++;
+            Console.WriteLine(
+                $"{_counter} - start --- {Thread.CurrentThread.GetHashCode()}");
+
+            Thread thread = new(Method);
+            thread.Start();
+            thread.Join();
+        }
+
+        Console.WriteLine($"Thread {Thread.CurrentThread.GetHashCode()} was finished");
+    }
+}
