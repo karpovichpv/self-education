1175fa72dd0278f81e1ad5ead314155902d25b06
Pavel Karpovich <karpovich.pavel@gmail.com>
2024-02-24 09:54:14 +0300

    Add examples for thread priorities

---
+++ .net threads + async await/.net threads/ThreadPriority2/Program.cs
@@ -0,0 +1,62 @@
+﻿
+class PriorityTest
+{
+    public bool Stop = false;
+
+    public void Method()
+    {
+        Console.WriteLine(
+            $"Thread {Thread.CurrentThread.ManagedThreadId}" +
+            $" with priority {Thread.CurrentThread.Priority}" +
+            $"began work");
+
+        long count = 0;
+
+        while (!Stop)
+            count++;
+
+        Console.WriteLine(
+            $"Thread {Thread.CurrentThread.ManagedThreadId} with priority " +
+            $"{Thread.CurrentThread.Priority} finished. Count = {count:0}");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Press any key...");
+        Console.ReadKey();
+
+        Console.WriteLine(
+            $"Default priority of " +
+            $"the primary thread {Thread.CurrentThread.Priority}");
+
+        PriorityTest priorityTest = new();
+
+        int threadNumber = 4;
+        Thread[] threads = new Thread[threadNumber];
+        for (int i = 0; i < threadNumber; i++)
+            threads[i] = new Thread(priorityTest.Method);
+
+        threads[0].Priority = ThreadPriority.Lowest;
+
+        for (int i = 1; i < threadNumber; i++)
+            threads[i].Priority = ThreadPriority.Highest;
+
+        threads[0].Start();
+
+        Thread.Sleep(2000);
+
+        for (int i = 1; i < threadNumber; i++)
+            threads[i].Start();
+
+        Thread.Sleep(10000);
+
+        Console.WriteLine("The primary thread woke up and run");
+
+        priorityTest.Stop = true;
+
+        Console.ReadKey();
+    }
+}
