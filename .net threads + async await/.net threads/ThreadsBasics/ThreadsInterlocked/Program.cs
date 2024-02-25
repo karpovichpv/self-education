e4bd62c050aa9cf040a02bda9798221780c0848d
Pavel Karpovich <karpovich.pavel@gmail.com>
2024-02-25 11:58:31 +0300

    Finish with the threads basic course

---
+++ .net threads + async await/.net threads/ThreadsBasics/ThreadsInterlocked/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    private static long _counter;
+    private static object _block = new();
+
+    private static void Procedure()
+    {
+        for (int i = 0; i < 1000000; i++)
+        {
+            Interlocked.Increment(ref _counter);
+
+            //lock (_block)
+            //{
+            //    _counter++;
+            //}
+        }
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Expected counter value equals 10000000");
+
+        Thread[] threads = new Thread[10];
+
+        for (int i = 0; i < 10; ++i)
+        {
+            (threads[i] = new Thread(Procedure)).Start();
+        }
+
+        for (int i = 0; i < 10; ++i)
+            threads[i].Join();
+
+        Console.WriteLine($"Real counter value equals {_counter}");
+
+        Console.ReadKey();
+    }
+}
---
+++ .net threads + async await/.net threads/ThreadsBasics/ThreadsSpinLock/Program.cs
@@ -0,0 +1,90 @@
+﻿public class SpinLock
+{
+    // flag [0 - free; 1 - block is busy]
+    private int _block;
+
+    // Checking interval
+    private int _wait;
+
+    public SpinLock(int wait)
+    {
+        _wait = wait;
+    }
+
+    // Set lock
+    public void Enter()
+    {
+        int result = Interlocked.CompareExchange(ref _block, 1, 0);
+
+        while (result == 1)
+        {
+            Thread.Sleep(_wait);
+            result = Interlocked.CompareExchange(ref _block, 1, 0);
+        }
+    }
+
+    // Reset lock
+    public void Exit()
+    {
+        Interlocked.Exchange(ref _block, 0);
+    }
+}
+
+public class SpinLockManager : IDisposable
+{
+    private SpinLock _block;
+
+    public SpinLockManager(SpinLock block)
+    {
+        _block = block;
+        block.Enter();
+    }
+
+    public void Dispose()
+    {
+        _block.Exit();
+    }
+}
+
+class Program
+{
+    private static Random _random = new();
+    private static SpinLock _block = new(10);
+
+    private static FileStream _stream = File.Open(
+        "log.txt",
+        FileMode.Create,
+        FileAccess.Write,
+        FileShare.None);
+    private static StreamWriter _writer = new StreamWriter(_stream);
+
+    private static void Function()
+    {
+        using (new SpinLockManager(_block))
+        {
+            _writer.WriteLine($"Thread {Thread.CurrentThread.GetHashCode()} is starting");
+            _writer.Flush();
+        }
+
+        int time = _random.Next(10, 200);
+        Thread.Sleep(time);
+
+        using (new SpinLockManager(_block))
+        {
+            _writer.WriteLine($"Thread [{Thread.CurrentThread.GetHashCode()}] is finishing");
+            _writer.Flush();
+        }
+    }
+
+    public static void Main()
+    {
+        Thread[] threads = new Thread[50];
+
+        for (uint i = 0; i < 50; ++i)
+        {
+            threads[i] = new Thread(Function);
+            threads[i].Start();
+        }
+    }
+}
+
