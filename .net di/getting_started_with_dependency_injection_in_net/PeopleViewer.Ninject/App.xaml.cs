8aceee22cdc42e89595da5d340437807c69ad202
Pavel Karpovich <66169282+karpovichpv@users.noreply.github.com>
2024-08-10 10:35:43 +0300

    Threads examples (#4)
    * Add the second ThreadJoinMethod2
    
    * Add an example of accessing to the static field from the thread
    
    * Add ThreadAbort project
    
    * Add ThreadBackground example project
    
    * Add examples for thread priorities
    
    * Finish with the threads basic course
    
    * Add examples for Threads
    
    * Add the second example for the volatile keyword
    
    * Add ThreadPool example
    
    * Add an example for mutex realization
    
    * Add loads for examples for thread sync
    
    * Examples for async operations
    
    * Add TPL, Parallel, PLINQ examples
    
    * Add async await examples
    
    * Start unit test course
    
    * Add the course about dependecy injection
    
    ---------
    
    Co-authored-by: Pavel Karpovich <karpovich.pavel@gmail.com>

---
+++ .net di/getting_started_with_dependency_injection_in_net/PeopleViewer.Ninject/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Ninject;
+using PeopleViewer.Common;
+using PersonDataReader.Decorators;
+using PersonDataReader.Service;
+using System.Windows;
+
+namespace PeopleViewer.Ninject
+{
+	public partial class App : Application
+	{
+		private IKernel Container = new StandardKernel();
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+			ConfigureContainer();
+			ComposeObjects();
+			Application.Current.MainWindow.Title = "With Dependency Injection - Ninject";
+			Application.Current.MainWindow.Show();
+		}
+
+		private void ConfigureContainer()
+		{
+			Container.Bind<IPersonReader>().To<CachingReader>()
+				.InSingletonScope()
+				.WithConstructorArgument<IPersonReader>(Container.Get<ServiceReader>());
+		}
+
+		private void ComposeObjects()
+		{
+			Application.Current.MainWindow = Container.Get<PeopleViewerWindow>();
+		}
+	}
+}
---
+++ .net di/getting_started_with_dependency_injection_in_net/PeopleViewer.View/PeopleViewerWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using PeopleViewer.Presentation;
+using System.Windows;
+
+namespace PeopleViewer
+{
+	public partial class PeopleViewerWindow : Window
+	{
+		readonly PeopleViewModel _viewModel;
+
+		public PeopleViewerWindow(PeopleViewModel viewModel)
+		{
+			InitializeComponent();
+			this.DataContext = viewModel;
+			_viewModel = viewModel;
+		}
+
+		private void FetchButton_Click(object sender, RoutedEventArgs e)
+		{
+			_viewModel.RefreshPeople();
+			ShowRepositoryType();
+		}
+
+		private void ClearButton_Click(object sender, RoutedEventArgs e)
+		{
+			_viewModel.ClearPeople();
+			ClearRepositoryType();
+		}
+
+		private void ShowRepositoryType()
+		{
+			RepositoryTypeTextBlock.Text = _viewModel.DataReaderType;
+		}
+
+		private void ClearRepositoryType()
+		{
+			RepositoryTypeTextBlock.Text = string.Empty;
+		}
+	}
+}
