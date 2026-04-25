f10897504aca4af2006a513d64c0ce5574404f23
Pavel Karpovich <karpovich.pavel@gmail.com>
2026-04-25 16:51:35 +0300

    add: a few lessons

---
+++ js/course 2/010_variables_naming/index.js
@@ -0,0 +1,8 @@
+const vehicleBodyWidth = 5000;
+const vehicleBodyLenght = 4000;
+
+console.log("Ширина кузова автомобиля" + vehicleBodyWidth + ", длина" + vehicleBodyLenght);
+
+['vruru', 'ecrver', 'vvv'].map(item =>)
+
+// Request
---
+++ js/course 2/013_user_input_simple_1/js/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+
+const arr = ['a', 'b', 'c'];
+const arrObj = {
+    0: 'a',
+    1: 'b',
+    2: 'c',
+};
+
+arrObj['b'] = '1234';
+console.log(arr);
+console.log(arrObj[1]);
+console.log(arrObj.b);
+
+//const obj = { a: 1, b: 2 };
+const obj = {
+    'Anna': 500,
+    'Alice': 800
+};
+
+
+
