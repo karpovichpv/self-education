a6dae85bd667be1a77123b5611b40890960658cc
Pavel Karpovich <karpovich.pavel@gmail.com>
2026-04-29 09:07:47 +0300

    feat: add an oop lesson

--- js/course 2/035_spread_operator/js/index.js
+++ js/course 2/035_spread_operator/js/index.js
@@ -1,82 +1,29 @@
 "use strict";
 
-let a = 6;
-let b = a;
+let str = 'some string';
+let strObj = new String(str);
 
-b += 5;
-console.log(b);
-console.log(a);
+//console.log(str);
+//console.log(strObj);
+//console.dir([1, 2, 3]);
 
-
-const obj = {
-    a: 5,
-    b: 1
-};
-
-copy.a = 10;
-console.log(obj);
-
-function copy(obj) {
-    let newObj = {};
-    let key;
-    for (key in obj) {
-        newObj[key] = obj[key];
-    }
-
-    return newObj;
-}
-
-const testObj = {
-    a: 5,
-    b: 1,
-    c: {
-        x: 7,
-        y: 4,
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function () {
+        console.log("hello!");
     }
 };
 
-console.log(copy(testObj));
-
-const add = {
-    d: 17,
-    e: 20
+const john = {
+    health: 100
 };
 
-console.log(Object.assign(testObj, add));
-var clonedElement = Object.assign(testObj, add);
-clonedElement.d = 3330;
+john.__proto__ = soldier;
 
-console.log(clonedElement);
+const paul = Object.create(soldier);
 
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
+console.log(john.armor);
+console.log(john.sayHello());
 
-console.log(newArray);
-console.log(oldArray);
-
-const video = ['youtube', 'vimeo', 'rutube'];
-const blogs = ['wordpress', 'livejournal', 'blogger'];
-
-const internet = [...video, ...blogs, 'vk', 'facebook'];
-console.log(internet);
-
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-
-const num = [2, 5, 7];
-
-log(...num);
-
-const array = ['a', 'b', 'c'];
-const array1 = [...array];
-console.log(array1);
-
-const q = {
-    one: 1,
-    two: 2,
-};
-
-console.log({ ...q });
+paul.sayHello();
