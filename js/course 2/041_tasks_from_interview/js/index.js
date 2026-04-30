d43c27b6fa3f3788ace5b2cbc2c7228160d75cab
karpovichpv <karpovich.pavel@gmail.com>
2026-04-30 22:39:31 +0300

    feat: add tasks from interview

---
+++ js/course 2/041_tasks_from_interview/js/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// 1
+let x = 5;
+console.log(x++);
+
+// 2 
+const value1 = [] + false - null + true;
+console.log(value1);
+
+let y = 1; let x = y = 2; console.log(x);
+
+console.log([] + 1 + 2);
+console.log("1"[0])
+
+console.log(2 && 1 && null && 0 && undefined);
+
+console.log(1&&2);
+console.log(!!(1&&2));
