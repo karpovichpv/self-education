cc1cd93f28b722905e0688557c2fa3c88b231051
Pavel Karpovich <karpovich.pavel@gmail.com>
2026-05-04 13:06:06 +0300

    feat: add a loading src lesson

---
+++ js/course 2/050_async_defer/js/script.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const p = document.querySelectorAll('p');
+console.log(p);
+
+function loadScript(src) {
+    const script = document.createElement('script')
+    script.src = src
+    script.async = false;
+    document.body.append(script)
+}
+
