4615c1acb8662f898d96dc5b063e3a22034a1b16
karpovichpv <karpovich.pavel@gmail.com>
2026-05-04 21:10:57 +0300

    feat: add an optional chaining operator lesson

---
+++ js/course 2/054_optional_chaining_operator/js/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const box = document.querySelector('.box')
+const block = document.querySelector('.block')
+
+console.log(block);
+console.log(block?.textContent);
+console.log(1 + 3);
