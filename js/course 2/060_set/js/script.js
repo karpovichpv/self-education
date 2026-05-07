4e5d8bc339c46dcac95cce2f1236df2dc7ea3db2
Pavel Karpovich <karpovich.pavel@gmail.com>
2026-05-07 13:08:00 +0300

    feat: add a set lesson

---
+++ js/course 2/060_set/js/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const arr = ['Alex', 'Ann', 'Oleg', 'Alex']
+
+const set = new Set(arr)
+
+set.add('Ivan')
+    .add('Oleg')
+    .add('Ivan');
+
+console.log(set);
+
+// set.delete(value);
+// set.has(value);
+// set.clear();
+// set.size;
+
+for (let value of set)
+    console.log(value);
+
+set.forEach((value, valueAgain, set) => {
+    console.log(value, valueAgain);
+})
+
+console.log(set.values());
+console.log(set.keys());
+console.log(set.entries());
+
+function unique(arr) {
+    return Array.from(new Set(arr))
+}
+
+console.log(unique(['ann', 'ann']));
